8a91931d8a232a72b93a51859ac87675f51a461d
jonusis <1624100548@qq.com>
2021-03-21 19:25:21 +0800

    feat:new page:first page

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import FirstPage from './pages/firstPage/index';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FirstPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ app/src/pages/firstPage/index.js
@@ -0,0 +1,86 @@
+import React,{ useState } from 'react';
+// import Header from '../../compoments/header/index'
+// import Banner from '../../compoments/banner/index'
+import imageU from '../../resorse/学习经历_slices/logo.png';
+import imageG from '../../resorse/学习资料_slices/logo.png';
+import imageB from '../../resorse/learn 机器学习_slices/logo.png';
+import imageC from '../../resorse/项目_slices/logo.png'
+import imageH from '../../resorse/合作_slices/logo.png'
+
+import './index.css';
+
+function FirstPage() {
+  const [pageTitle,setPageTitle] = useState('半监督学习');
+  const [pageText,setPageText] = useState("在监督学习中，由于对大量数据都进行标注成本往往太高，所以只对其中一部分数据进行人工标注得到训练集，而用少量有标签样本数据训练的出分类模型往往并不具备足够强泛化能力较弱。此外，海量的无标签数据中又蕴含一些很有价值的信息，对海量的无标签样本数据完全不加利用显得太过浪费。为了解决这个问题,半监督学习的方法应运而生。21 世纪以前，由于计算机的存储性能以及处理性能上的不足，再加上统计学理论并不完善，半监督学习发展较慢。进入 21 世纪之后，半监督学习逐渐成为机器学习领域的重点研究方向之一。");
+  const [pageResourse,setpageResourse] = useState('资料来源：《基于主动自步学习的文本分类研究》');
+    return (
+      <div className="FirstPage">
+        <div className="box">
+          <div className="introduction">
+            <div className="title" onClick = {() =>{
+              setPageText('abc');
+              setPageTitle('半监督学习');
+              setpageResourse('资料来源：《基于主动自步学习的文本分类研究》');
+            }}>
+              <img src={imageU} className="titleImg" alt=""/>
+              <div className="wordStyle"> 半监督学习</div>
+              <div className="signPos1"> > </div>
+            </div>
+            <div className="title" onClick = {() =>{
+              setPageText('abc');
+              setPageTitle('人工神经网络');
+              setpageResourse('资料来源：《基于主动自步学习的文本分类研究》');
+            }}>
+              <img src={imageG} className="titleImg" alt=""/>
+              <div className="wordStyle"> 人工神经网络</div>
+              <div className="signPos2"> > </div>
+            </div>
+            <div className="title" onClick = {() =>{
+              setPageText('abc');
+              setPageTitle('Bert');
+              setpageResourse('资料来源：《基于主动自步学习的文本分类研究》');
+            }}>
+              <img src={imageB} className="titleImg" alt=""/>
+              <div className="wordStyle"> Bert</div>
+              <div className="signPos3"> > </div>
+            </div>
+          </div>
+          <div className="description">
+            <div className="desTitle">{pageTitle}</div>
+            <div className="desTextline">{pageText}</div>
+            <div className="desResourse">{pageResourse}</div>
+          </div>
+        </div>
+        <div className="feature">
+          <div className="backWord">Project Features</div>
+          <div className="featureTitle">
+            <img src={imageC} className="titleImage" alt=''/>
+            <div className="titleText">项目特色</div>
+          </div>
+          <div className="featureText">
+          <div>语言的发展是社会发展的表现形式，语言文明是社会文明的重要体现。“使网络空间清朗起来”更是习近平总书记多次提及的互联网政治目标。加强对网络不良语言信息的检测与管理无疑是整治网络整体环境的有效突破口。然而目前对于网络不良语言信息检测的研究相对较少，同时网络上海量的媒体内容使得不良语言信息分析，过滤与监测工作量巨大。如果仅仅使用少量的有标记的不良语言标记样本，利用它们训练出来的学习系统往往很难具有泛化能力。</div>
+          <div>因此，本项目旨在： </div>
+          <div>+ 搭建网络不良语言信息监测仿真平台，建立完善的相关API接口调用，为建设网络环境不良文本检测平台提供一套可行的方案； </div>
+          <div>+ 研究基于半监督学习的网络不良语言自动监测方法，迅速，有效的实现对于网络长文本短文本中存在的不良语言信息进行自动学习和识别。 </div>
+          <div>+ 构建不良语言词典和网络不良语料库，为网络不良语言信息的监测和应用技术提供支持。</div>
+          <div>+ 克服现有条件对网络舆论检测的局限，迅速有效的实现对网络中不良语言的自动学习与识别。在保证准确率的同时降低人工检测成本，并可识别由于缺乏先验知识、难以人工标注的不良语言。 </div>
+          <div>+ 经过半监督学习，构建科学合理且可动态扩展的不良语言语料库，实现不良语言的动态监测和统计分析，为网络不良语言的管理提供一些参考与帮助。 本网站为服务该项目搭建的平台。同时对于最新机器学习相关新闻与分享进行二次组织与深度聚合，从而有效提升信息服务质量，服务于学习者的信息获取需求； 网站首期提供的分类器包括：Centroid算法，FastText，TextCNN，bert等，同时创造性的使用基于图结构的KNN算法；涉及半监督算法有：简单自训练，MixMatch等。 </div>
+          <div>注：本项目定义不良文本为含有色情、反动、辱骂信息的文本。</div>
+          </div>
+        </div>
+        <div className="feature">
+          <div className="backWord">Project Features</div>
+          <div className="featureTitle">
+            <img src={imageH} className="titleImage" alt=''/>
+            <div className="titleText">开发感言</div>
+          </div>
+          <div className="featureText idt">
+          <div>在项目开发过程中感谢沈显君老师的支持与鼓励，同时也要感谢学长学姐对本项目的教导和帮助。 通过本项目，我们深刻地体会到了团队协作的重要性以及自己知识的不足。在此次开发过程中， 我们学到了许多，认识到了与更优秀的计算机人之间存在的差距。</div>
+          </div>
+        </div>
+      </div>
+
+    );
+  }
+
+export default FirstPage;
